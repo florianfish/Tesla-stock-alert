f21f0128256d32efe39cc6fb771dbaeae90d36d6
Florian Poisson <florian.poisson@6tm.com>
2023-08-03 11:44:25 +0200

    improve

--- teslajson.py
+++ teslajson.py
@@ -22,7 +22,7 @@ telegram_chat_id = config("TELEGRAM_CHAT_ID")
 telegram_token = config("TELEGRAM_API_TOKEN")
 
 urls = {
-    "35000": 'https://www.tesla.com/inventory/api/v1/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22m3%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22LRRWD%22%2C%22M3RWD%22%5D%2C%22PAINT%22%3A%5B%22GRAY%22%2C%22WHITE%22%2C%22BLUE%22%5D%7D%2C%22arrangeby%22%3A%22Price%22%2C%22order%22%3A%22asc%22%2C%22market%22%3A%22FR%22%2C%22language%22%3A%22fr%22%2C%22super_region%22%3A%22north%20america%22%2C%22lng%22%3A-1.7075198%2C%22lat%22%3A48.11734209999999%2C%22zip%22%3A%2235000%22%2C%22range%22%3A50%2C%22region%22%3A%22FR%22%7D%2C%22offset%22%3A0%2C%22count%22%3A50%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%7D',
+    "35000": 'https://www.tesla.com/inventory/api/v1/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22m3%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22LRRWD%22%2C%22M3RWD%22%5D%2C%22PAINT%22%3A%5B%22GRAY%22%2C%22WHITE%22%2C%22BLUE%22%5D%7D%2C%22arrangeby%22%3A%22Price%22%2C%22order%22%3A%22asc%22%2C%22market%22%3A%22FR%22%2C%22language%22%3A%22fr%22%2C%22super_region%22%3A%22north%20america%22%2C%22lng%22%3A-1.7075198%2C%22lat%22%3A48.11734209999999%2C%22zip%22%3A%2244000%22%2C%22range%22%3A50%2C%22region%22%3A%22FR%22%7D%2C%22offset%22%3A0%2C%22count%22%3A50%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%7D',
     "44000": 'https://www.tesla.com/inventory/api/v1/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22m3%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22LRRWD%22%2C%22M3RWD%22%5D%2C%22PAINT%22%3A%5B%22GRAY%22%2C%22WHITE%22%2C%22BLUE%22%5D%7D%2C%22arrangeby%22%3A%22Price%22%2C%22order%22%3A%22asc%22%2C%22market%22%3A%22FR%22%2C%22language%22%3A%22fr%22%2C%22super_region%22%3A%22north%20america%22%2C%22lng%22%3A-1.7075198%2C%22lat%22%3A48.11734209999999%2C%22zip%22%3A%2244000%22%2C%22range%22%3A50%2C%22region%22%3A%22FR%22%7D%2C%22offset%22%3A0%2C%22count%22%3A50%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%7D',
 }
 # Fonction pour envoyer une notif Telegram
@@ -44,6 +44,7 @@ for zipcode, url in urls.items():
             hash = vehicle.get("Hash", "")
             vin = vehicle.get("VIN", "")
             trim_name = vehicle.get("TrimName", "")
+            paint = vehicle.get("PAINT", "")
             in_transit = vehicle.get("InTransit", False)
             is_demo = bool(vehicle.get("IsDemo", False))
             is_at_location = bool(vehicle.get("IsAtLocation", False))
@@ -56,10 +57,10 @@ for zipcode, url in urls.items():
 
             if inventory_price <= PRICE_LIMIT and is_at_location is True and is_demo is False:
                 match = True
-                message = f"{trim_name} à {inventory_price} € >> {link}"
+                message = f"{trim_name} à {inventory_price} €, {paint} >> {link}"
                 send_telegram_notif(message)
             if print_all_results is True:
-                message_all_results = message_all_results + trim_name + " à " + str(inventory_price) + " €, En transit = " + str(in_transit) + ", Démonstration = " + str(is_demo) + " (" + link + ")\n"
+                message_all_results = message_all_results + trim_name + " à " + str(inventory_price) + " €, Couleur {paint}, En transit = " + str(in_transit) + ", Démonstration = " + str(is_demo) + " (" + link + ")\n"
         if match is False:
             print("Aucun résultat :'(")
     else:
